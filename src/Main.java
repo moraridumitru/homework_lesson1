a101d318ea76f5cab7dc22581a26056934198396
moraridumitru <moraridumitru1@gmail.com>
2023-05-21 22:26:02 +0300

    homework1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ package Home_Work_Lesson1;
 public class Main {
     public static void main(String[] args) {
         String fraza = "I love java";
-        boolean frazanoastra = fraza.equals("I love java"); /**
-         используя оператор сравнения с помощью метода equals()**/
-
+        boolean frazanoastra = fraza.equals("I love java");
+        /**используя оператор сравнения с помощью метода equals()
+         **/
         System.out.println("I love java - este de tip String: " + frazanoastra);
     }
 }
