cae7164ba8ba386d31b82ff2959e795bbca0090c
moraridumitru <133877747+moraridumitru@users.noreply.github.com>
2023-05-21 22:00:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fraza = "I love java";
+        boolean frazanoastra = fraza.equals("I love java"); /**
+         используя оператор сравнения с помощью метода equals()**/
+
+        System.out.println("I love java - este de tip String: " + frazanoastra);
     }
 }
